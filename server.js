4129ae09da7c4a1e5a084694c4837354ffd4f279
dpayton92 <dpayton92@gmail.com>
2020-09-15 11:51:34 -0700

    update server.js

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useFindAndModify: false
+})
+
+app.use("/api", require("../Get-Fit-Workout-Tracker/routes/api.js"));
+app.use("/", require("../Get-Fit-Workout-Tracker/routes/html.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
+
